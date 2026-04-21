e1e4644bb19ab6035764b868e5f464832ac2cefc
nadson-alves <nadsonalves002@gmail.com>
2026-04-21 14:56:16 +0000

    mudanca para o modo mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,38 +5,65 @@
 int main() {
     int tabuleiro[10][10];
 
-    // preenchimento com 0
+    // preencher com 0
     for(int i = 0; i < 10; i++) {
         for(int j = 0; j < 10; j++) {
             tabuleiro[i][j] = 0;
         }
     }
 
-    // navio diagonal (↘) começando "salteado"
-    int linha1 = 1;
-    int coluna1 = 3;
-    for(int k = 0; k < 3; k++) {
-        tabuleiro[linha1 + k][coluna1 + k] = 3;
+    // ====== CRUZ ======
+
+    // coluna vertical (centro)
+    for(int i = 0; i <= 4; i++) {
+        tabuleiro[i][4] = 3;
     }
 
-    // navio diagonal (↙) começando "salteado"
-    int linha2 = 2;
-    int coluna2 = 8;
-    for(int k = 0; k < 3; k++) {
-        tabuleiro[linha2 + k][coluna2 - k] = 3;
+    // linha horizontal
+    for(int j = 2; j <= 6; j++) {
+        tabuleiro[2][j] = 3;
     }
 
-    printf("Tabuleiro Batalha Naval:\n");
-    // imprimindo letras (colunas)
+    // ====== OCTAEDRA ======
+
+    tabuleiro[3][7] = 2;
+
+    for(int j = 6; j <= 8; j++) {
+        tabuleiro[4][j] = 2;
+    }
+
+    for(int j = 5; j <= 8; j++) {
+        tabuleiro[5][j] = 2;
+    }
+
+    tabuleiro[6][6] = 2;
+    tabuleiro[6][7] = 2;
+
+    tabuleiro[7][7] = 2;
+
+    // ====== CONE ======
+
+    tabuleiro[5][2] = 1;
+
+    tabuleiro[6][1] = 1;
+    tabuleiro[6][2] = 1;
+    tabuleiro[6][3] = 1;
+
+    for(int j = 0; j <= 4; j++) {
+        tabuleiro[7][j] = 1;
+    }
+
+    // ===== impressão =====
+
+    printf("Batalha Naval : \n");
     printf("   ");
     for(int j = 0; j < 10; j++) {
-        printf("%c ", 'A' + j);
+        printf("%d ", j);
     }
     printf("\n");
 
-    // imprimindo tabuleiro
     for(int i = 0; i < 10; i++) {
-        printf("%2d ", i);
+        printf("%d  ", i);
         for(int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
